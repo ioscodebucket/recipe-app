c9d83727ee40ac78a2fdbe06610884776f49af07
NadjaK <nadjakelly888@gmail.com>
2022-08-08 14:08:18 +0200

    Initial Commit

---
+++ RecipeApp2/RecipeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp2App.swift
+//  RecipeApp2
+//
+//  Created by Nadja Kelly on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
