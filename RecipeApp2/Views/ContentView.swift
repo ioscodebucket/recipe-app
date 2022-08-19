7eeb6cd13a84101fb5369749342b24bba5b94cb7
NadjaK <nadjakelly888@gmail.com>
2022-08-19 17:57:55 +0200

    change swiftUI list to ScrollView and ForEach hide NavigationBarTitle and use text element for the title in RecipeListView

--- RecipeApp2/Views/ContentView.swift
+++ RecipeApp2/Views/ContentView.swift
@@ -8,45 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-   // @ObservedObject var model = RecipeModel()
+    // @ObservedObject var model = RecipeModel()
     
     @EnvironmentObject var model: RecipeModel
     
     var body: some View {
-
+        
+        
+        
+        
         NavigationView {
-            List(model.recipes) { recipiesItems in
-                
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: recipiesItems),
-                    label: {
-                    
-                    //MARK: Row item
-                        HStack (spacing: 20 ){
-                            Image(recipiesItems.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .clipped()
-                        .cornerRadius(5)
-                    
-                    Text(recipiesItems.name)
-                            
-                            
-   
-                        
-                    
             
-                }//:HStack image
-                })
+            
+            VStack(alignment: .leading){
+                //
+                Text("All Recipes")
+                    .bold()
+                    //.padding(.leading)
+                    .padding(.top, 40)
+                    .font(.largeTitle)
                 
-            }//: List
-            .navigationBarTitle("All Recipes")
+                
+                ScrollView{
+                    LazyVStack(alignment: .leading){
+                        ForEach(model.recipes) { recipiesItems in
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: recipiesItems),
+                                label: {
+                                    
+                                    //MARK: Row item
+                                    HStack (spacing: 20 ){
+                                        Image(recipiesItems.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        
+                                        Text(recipiesItems.name)
+                                            .foregroundColor(.white)
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                    }//:HStack image
+                                })
+                            
+                        }//: List
+                    }//:LazyVStack
+                }//:SrollView
+            }//:VStack
+            .padding(.leading)
+            .navigationBarHidden(true)
+            //.navigationBarTitle("All Recipes")
             
             
         }//:NavigationView
         
-    
+        
         
     }//HStack body
     
@@ -55,5 +77,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(RecipeModel())
     }
 }
